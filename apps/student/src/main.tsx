3298e833e61e5ca5c144aa86d411dc9b98d18cc8
unluckyidiot16 <unluckyidiot16@gmail.com>
2025-10-15 14:00:41 +0900

    Update main.tsx

--- apps/student/src/main.tsx
+++ apps/student/src/main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import './sw-register';
+//import './sw-register';
 import Main from './views/Main';
 import Lobby from './views/Lobby';
 import Play from './views/Play';
